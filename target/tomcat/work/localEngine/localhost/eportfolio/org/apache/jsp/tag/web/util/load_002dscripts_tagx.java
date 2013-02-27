04e7b425b9f475bb95b0001667a2590b72ed28f8
qgeffard <q.geffard@gmail.com>
2013-02-27 11:52:47 +0100

    INtegration plugin Ckeditor pour la partie Administration

--- target/tomcat/work/localEngine/localhost/eportfolio/org/apache/jsp/tag/web/util/load_002dscripts_tagx.java
+++ target/tomcat/work/localEngine/localhost/eportfolio/org/apache/jsp/tag/web/util/load_002dscripts_tagx.java
@@ -102,6 +102,13 @@ static {
         return;
       if (_jspx_meth_spring_005furl_005f10(_jspx_page_context))
         return;
+      if (_jspx_meth_spring_005furl_005f11(_jspx_page_context))
+        return;
+      out.write("<script src=\"" + (java.lang.String) org.apache.jasper.runtime.PageContextImpl.proprietaryEvaluate("${ckeditor_url}", java.lang.String.class, (PageContext)this.getJspContext(), null, false) + "\" type=\"text/javascript\">");
+      out.write('b');
+      out.write('i');
+      out.write('d');
+      out.write("</script>");
       out.write("<script src=\"" + (java.lang.String) org.apache.jasper.runtime.PageContextImpl.proprietaryEvaluate("${jquery_url}", java.lang.String.class, (PageContext)this.getJspContext(), null, false) + "\" type=\"text/javascript\">");
       out.write('b');
       out.write('i');
@@ -379,10 +386,10 @@ static {
     //  spring:url
     org.springframework.web.servlet.tags.UrlTag _jspx_th_spring_005furl_005f7 = (org.springframework.web.servlet.tags.UrlTag) _005fjspx_005ftagPool_005fspring_005furl_0026_005fvar_005fvalue_005fnobody.get(org.springframework.web.servlet.tags.UrlTag.class);
     _jspx_th_spring_005furl_005f7.setPageContext(_jspx_page_context);
-    _jspx_th_spring_005furl_005f7.setParent(new javax.servlet.jsp.tagext.TagAdapter((javax.servlet.jsp.tagext.SimpleTag) this ));    // /WEB-INF/tags/util/load-scripts.tagx(24,102) name = var type = null reqTime = true required = false fragment = false deferredValue = false expectedTypeName = null deferredMethod = false methodSignature = null
-    _jspx_th_spring_005furl_005f7.setVar("jquery_url");
-    // /WEB-INF/tags/util/load-scripts.tagx(24,102) name = value type = null reqTime = true required = true fragment = false deferredValue = false expectedTypeName = null deferredMethod = false methodSignature = null
-    _jspx_th_spring_005furl_005f7.setValue("/resources/plugin/jquery-ui-1.9.2.custom/js/jquery-1.8.3.js");
+    _jspx_th_spring_005furl_005f7.setParent(new javax.servlet.jsp.tagext.TagAdapter((javax.servlet.jsp.tagext.SimpleTag) this ));    // /WEB-INF/tags/util/load-scripts.tagx(18,82) name = var type = null reqTime = true required = false fragment = false deferredValue = false expectedTypeName = null deferredMethod = false methodSignature = null
+    _jspx_th_spring_005furl_005f7.setVar("ckeditor_url");
+    // /WEB-INF/tags/util/load-scripts.tagx(18,82) name = value type = null reqTime = true required = true fragment = false deferredValue = false expectedTypeName = null deferredMethod = false methodSignature = null
+    _jspx_th_spring_005furl_005f7.setValue("/resources/plugin/ckeditor/ckeditor.js");
     int[] _jspx_push_body_count_spring_005furl_005f7 = new int[] { 0 };
     try {
       int _jspx_eval_spring_005furl_005f7 = _jspx_th_spring_005furl_005f7.doStartTag();
@@ -406,10 +413,10 @@ static {
     //  spring:url
     org.springframework.web.servlet.tags.UrlTag _jspx_th_spring_005furl_005f8 = (org.springframework.web.servlet.tags.UrlTag) _005fjspx_005ftagPool_005fspring_005furl_0026_005fvar_005fvalue_005fnobody.get(org.springframework.web.servlet.tags.UrlTag.class);
     _jspx_th_spring_005furl_005f8.setPageContext(_jspx_page_context);
-    _jspx_th_spring_005furl_005f8.setParent(new javax.servlet.jsp.tagext.TagAdapter((javax.servlet.jsp.tagext.SimpleTag) this ));    // /WEB-INF/tags/util/load-scripts.tagx(25,114) name = var type = null reqTime = true required = false fragment = false deferredValue = false expectedTypeName = null deferredMethod = false methodSignature = null
-    _jspx_th_spring_005furl_005f8.setVar("jqueryUi_url");
-    // /WEB-INF/tags/util/load-scripts.tagx(25,114) name = value type = null reqTime = true required = true fragment = false deferredValue = false expectedTypeName = null deferredMethod = false methodSignature = null
-    _jspx_th_spring_005furl_005f8.setValue("/resources/plugin/jquery-ui-1.9.2.custom/js/jquery-ui-1.9.2.custom.js");
+    _jspx_th_spring_005furl_005f8.setParent(new javax.servlet.jsp.tagext.TagAdapter((javax.servlet.jsp.tagext.SimpleTag) this ));    // /WEB-INF/tags/util/load-scripts.tagx(27,102) name = var type = null reqTime = true required = false fragment = false deferredValue = false expectedTypeName = null deferredMethod = false methodSignature = null
+    _jspx_th_spring_005furl_005f8.setVar("jquery_url");
+    // /WEB-INF/tags/util/load-scripts.tagx(27,102) name = value type = null reqTime = true required = true fragment = false deferredValue = false expectedTypeName = null deferredMethod = false methodSignature = null
+    _jspx_th_spring_005furl_005f8.setValue("/resources/plugin/jquery-ui-1.9.2.custom/js/jquery-1.8.3.js");
     int[] _jspx_push_body_count_spring_005furl_005f8 = new int[] { 0 };
     try {
       int _jspx_eval_spring_005furl_005f8 = _jspx_th_spring_005furl_005f8.doStartTag();
@@ -433,10 +440,10 @@ static {
     //  spring:url
     org.springframework.web.servlet.tags.UrlTag _jspx_th_spring_005furl_005f9 = (org.springframework.web.servlet.tags.UrlTag) _005fjspx_005ftagPool_005fspring_005furl_0026_005fvar_005fvalue_005fnobody.get(org.springframework.web.servlet.tags.UrlTag.class);
     _jspx_th_spring_005furl_005f9.setPageContext(_jspx_page_context);
-    _jspx_th_spring_005furl_005f9.setParent(new javax.servlet.jsp.tagext.TagAdapter((javax.servlet.jsp.tagext.SimpleTag) this ));    // /WEB-INF/tags/util/load-scripts.tagx(26,100) name = var type = null reqTime = true required = false fragment = false deferredValue = false expectedTypeName = null deferredMethod = false methodSignature = null
-    _jspx_th_spring_005furl_005f9.setVar("previewJs_url");
-    // /WEB-INF/tags/util/load-scripts.tagx(26,100) name = value type = null reqTime = true required = true fragment = false deferredValue = false expectedTypeName = null deferredMethod = false methodSignature = null
-    _jspx_th_spring_005furl_005f9.setValue("/resources/plugin/jquery-ui-1.9.2.custom/js/preview.js");
+    _jspx_th_spring_005furl_005f9.setParent(new javax.servlet.jsp.tagext.TagAdapter((javax.servlet.jsp.tagext.SimpleTag) this ));    // /WEB-INF/tags/util/load-scripts.tagx(28,114) name = var type = null reqTime = true required = false fragment = false deferredValue = false expectedTypeName = null deferredMethod = false methodSignature = null
+    _jspx_th_spring_005furl_005f9.setVar("jqueryUi_url");
+    // /WEB-INF/tags/util/load-scripts.tagx(28,114) name = value type = null reqTime = true required = true fragment = false deferredValue = false expectedTypeName = null deferredMethod = false methodSignature = null
+    _jspx_th_spring_005furl_005f9.setValue("/resources/plugin/jquery-ui-1.9.2.custom/js/jquery-ui-1.9.2.custom.js");
     int[] _jspx_push_body_count_spring_005furl_005f9 = new int[] { 0 };
     try {
       int _jspx_eval_spring_005furl_005f9 = _jspx_th_spring_005furl_005f9.doStartTag();
@@ -460,10 +467,10 @@ static {
     //  spring:url
     org.springframework.web.servlet.tags.UrlTag _jspx_th_spring_005furl_005f10 = (org.springframework.web.servlet.tags.UrlTag) _005fjspx_005ftagPool_005fspring_005furl_0026_005fvar_005fvalue_005fnobody.get(org.springframework.web.servlet.tags.UrlTag.class);
     _jspx_th_spring_005furl_005f10.setPageContext(_jspx_page_context);
-    _jspx_th_spring_005furl_005f10.setParent(new javax.servlet.jsp.tagext.TagAdapter((javax.servlet.jsp.tagext.SimpleTag) this ));    // /WEB-INF/tags/util/load-scripts.tagx(27,94) name = var type = null reqTime = true required = false fragment = false deferredValue = false expectedTypeName = null deferredMethod = false methodSignature = null
-    _jspx_th_spring_005furl_005f10.setVar("menuJs_url");
-    // /WEB-INF/tags/util/load-scripts.tagx(27,94) name = value type = null reqTime = true required = true fragment = false deferredValue = false expectedTypeName = null deferredMethod = false methodSignature = null
-    _jspx_th_spring_005furl_005f10.setValue("/resources/plugin/jquery-ui-1.9.2.custom/js/menu.js");
+    _jspx_th_spring_005furl_005f10.setParent(new javax.servlet.jsp.tagext.TagAdapter((javax.servlet.jsp.tagext.SimpleTag) this ));    // /WEB-INF/tags/util/load-scripts.tagx(29,100) name = var type = null reqTime = true required = false fragment = false deferredValue = false expectedTypeName = null deferredMethod = false methodSignature = null
+    _jspx_th_spring_005furl_005f10.setVar("previewJs_url");
+    // /WEB-INF/tags/util/load-scripts.tagx(29,100) name = value type = null reqTime = true required = true fragment = false deferredValue = false expectedTypeName = null deferredMethod = false methodSignature = null
+    _jspx_th_spring_005furl_005f10.setValue("/resources/plugin/jquery-ui-1.9.2.custom/js/preview.js");
     int[] _jspx_push_body_count_spring_005furl_005f10 = new int[] { 0 };
     try {
       int _jspx_eval_spring_005furl_005f10 = _jspx_th_spring_005furl_005f10.doStartTag();
@@ -481,13 +488,40 @@ static {
     return false;
   }
 
+  private boolean _jspx_meth_spring_005furl_005f11(PageContext _jspx_page_context)
+          throws Throwable {
+    JspWriter out = _jspx_page_context.getOut();
+    //  spring:url
+    org.springframework.web.servlet.tags.UrlTag _jspx_th_spring_005furl_005f11 = (org.springframework.web.servlet.tags.UrlTag) _005fjspx_005ftagPool_005fspring_005furl_0026_005fvar_005fvalue_005fnobody.get(org.springframework.web.servlet.tags.UrlTag.class);
+    _jspx_th_spring_005furl_005f11.setPageContext(_jspx_page_context);
+    _jspx_th_spring_005furl_005f11.setParent(new javax.servlet.jsp.tagext.TagAdapter((javax.servlet.jsp.tagext.SimpleTag) this ));    // /WEB-INF/tags/util/load-scripts.tagx(30,94) name = var type = null reqTime = true required = false fragment = false deferredValue = false expectedTypeName = null deferredMethod = false methodSignature = null
+    _jspx_th_spring_005furl_005f11.setVar("menuJs_url");
+    // /WEB-INF/tags/util/load-scripts.tagx(30,94) name = value type = null reqTime = true required = true fragment = false deferredValue = false expectedTypeName = null deferredMethod = false methodSignature = null
+    _jspx_th_spring_005furl_005f11.setValue("/resources/plugin/jquery-ui-1.9.2.custom/js/menu.js");
+    int[] _jspx_push_body_count_spring_005furl_005f11 = new int[] { 0 };
+    try {
+      int _jspx_eval_spring_005furl_005f11 = _jspx_th_spring_005furl_005f11.doStartTag();
+      if (_jspx_th_spring_005furl_005f11.doEndTag() == javax.servlet.jsp.tagext.Tag.SKIP_PAGE) {
+        throw new SkipPageException();
+      }
+    } catch (Throwable _jspx_exception) {
+      while (_jspx_push_body_count_spring_005furl_005f11[0]-- > 0)
+        out = _jspx_page_context.popBody();
+      _jspx_th_spring_005furl_005f11.doCatch(_jspx_exception);
+    } finally {
+      _jspx_th_spring_005furl_005f11.doFinally();
+      _005fjspx_005ftagPool_005fspring_005furl_0026_005fvar_005fvalue_005fnobody.reuse(_jspx_th_spring_005furl_005f11);
+    }
+    return false;
+  }
+
   private boolean _jspx_meth_c_005fset_005f0(PageContext _jspx_page_context)
           throws Throwable {
     JspWriter out = _jspx_page_context.getOut();
     //  c:set
     org.apache.taglibs.standard.tag.rt.core.SetTag _jspx_th_c_005fset_005f0 = (org.apache.taglibs.standard.tag.rt.core.SetTag) _005fjspx_005ftagPool_005fc_005fset_0026_005fvar.get(org.apache.taglibs.standard.tag.rt.core.SetTag.class);
     _jspx_th_c_005fset_005f0.setPageContext(_jspx_page_context);
-    _jspx_th_c_005fset_005f0.setParent(new javax.servlet.jsp.tagext.TagAdapter((javax.servlet.jsp.tagext.SimpleTag) this ));    // /WEB-INF/tags/util/load-scripts.tagx(34,27) name = var type = java.lang.String reqTime = false required = false fragment = false deferredValue = false expectedTypeName = null deferredMethod = false methodSignature = null
+    _jspx_th_c_005fset_005f0.setParent(new javax.servlet.jsp.tagext.TagAdapter((javax.servlet.jsp.tagext.SimpleTag) this ));    // /WEB-INF/tags/util/load-scripts.tagx(38,27) name = var type = java.lang.String reqTime = false required = false fragment = false deferredValue = false expectedTypeName = null deferredMethod = false methodSignature = null
     _jspx_th_c_005fset_005f0.setVar("userLocale");
     int _jspx_eval_c_005fset_005f0 = _jspx_th_c_005fset_005f0.doStartTag();
     if (_jspx_eval_c_005fset_005f0 != javax.servlet.jsp.tagext.Tag.SKIP_BODY) {
@@ -524,7 +558,7 @@ static {
     org.apache.taglibs.standard.tag.rt.core.SetTag _jspx_th_c_005fset_005f1 = (org.apache.taglibs.standard.tag.rt.core.SetTag) _005fjspx_005ftagPool_005fc_005fset_0026_005fvar.get(org.apache.taglibs.standard.tag.rt.core.SetTag.class);
     _jspx_th_c_005fset_005f1.setPageContext(_jspx_page_context);
     _jspx_th_c_005fset_005f1.setParent((javax.servlet.jsp.tagext.Tag) _jspx_th_c_005fset_005f0);
-    // /WEB-INF/tags/util/load-scripts.tagx(35,26) name = var type = java.lang.String reqTime = false required = false fragment = false deferredValue = false expectedTypeName = null deferredMethod = false methodSignature = null
+    // /WEB-INF/tags/util/load-scripts.tagx(39,26) name = var type = java.lang.String reqTime = false required = false fragment = false deferredValue = false expectedTypeName = null deferredMethod = false methodSignature = null
     _jspx_th_c_005fset_005f1.setVar("plocale");
     int _jspx_eval_c_005fset_005f1 = _jspx_th_c_005fset_005f1.doStartTag();
     if (_jspx_eval_c_005fset_005f1 != javax.servlet.jsp.tagext.Tag.SKIP_BODY) {
@@ -558,9 +592,9 @@ static {
     org.apache.taglibs.standard.tag.rt.core.OutTag _jspx_th_c_005fout_005f0 = (org.apache.taglibs.standard.tag.rt.core.OutTag) _005fjspx_005ftagPool_005fc_005fout_0026_005fvalue_005fdefault_005fnobody.get(org.apache.taglibs.standard.tag.rt.core.OutTag.class);
     _jspx_th_c_005fout_005f0.setPageContext(_jspx_page_context);
     _jspx_th_c_005fout_005f0.setParent((javax.servlet.jsp.tagext.Tag) _jspx_th_c_005fset_005f0);
-    // /WEB-INF/tags/util/load-scripts.tagx(36,68) name = default type = null reqTime = true required = false fragment = false deferredValue = false expectedTypeName = null deferredMethod = false methodSignature = null
+    // /WEB-INF/tags/util/load-scripts.tagx(40,68) name = default type = null reqTime = true required = false fragment = false deferredValue = false expectedTypeName = null deferredMethod = false methodSignature = null
     _jspx_th_c_005fout_005f0.setDefault("en");
-    // /WEB-INF/tags/util/load-scripts.tagx(36,68) name = value type = null reqTime = true required = true fragment = false deferredValue = false expectedTypeName = null deferredMethod = false methodSignature = null
+    // /WEB-INF/tags/util/load-scripts.tagx(40,68) name = value type = null reqTime = true required = true fragment = false deferredValue = false expectedTypeName = null deferredMethod = false methodSignature = null
     _jspx_th_c_005fout_005f0.setValue((java.lang.Object) org.apache.jasper.runtime.PageContextImpl.proprietaryEvaluate("${fn:replace(plocale, '_', '-')}", java.lang.Object.class, (PageContext)this.getJspContext(), _jspx_fnmap_0, false));
     int _jspx_eval_c_005fout_005f0 = _jspx_th_c_005fout_005f0.doStartTag();
     if (_jspx_th_c_005fout_005f0.doEndTag() == javax.servlet.jsp.tagext.Tag.SKIP_PAGE) {
